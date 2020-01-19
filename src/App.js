a4503fba7d1a1a6c23696aa12fa1b353803a768f
Ogbinaka Emmanuel <ogbinakaemmanuel@gmail.com>
2020-01-19 17:44:17 +0100

    Architecting and scafolding phase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        // { name: "Frankeistein", id: "234" },
+        // { name: "Dracula", id: "23" },
+        // { name: "Pompei", id: "er32" },
+        // { name: "Chockee", id: "23ew" },
+        // { name: "Zombie", id: "21wer" },
+        // { name: "Igbudu Monkey", id: "gh4" }
+      ]
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(formattedData => this.setState({ monsters: formattedData }));
+  }
+
+  render() {
+    return this.state.monsters.map(monster => (
+      <h3 align="center" key={monster.id}>
+        <CardList name = 'Emmanuel' />
+        {monster.name}
+      </h3>
+    ));
+  }
 }
 
 export default App;
