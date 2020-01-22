9476b47c9d38aabbc44174ae9510359919ff1b1f
Ogbinaka Emmanuel <ogbinakaemmanuel@gmail.com>
2020-01-22 07:56:49 +0100

    finally after the night toil, woke up this morning and magically made the parent div element not part of the cardlist component, whooa..

--- src/App.js
+++ src/App.js
@@ -6,30 +6,33 @@ class App extends React.Component {
   constructor() {
     super();
 
-    this.state = { monsters: [
-    { name: "Frankeistein", id: "234" },
-    { name: "Dracula", id: "23" },
-    { name: "Pompei", id: "er32" },
-    { name: "Chockee", id: "23ew" },
-    { name: "Zombie", id: "21wer" },
-    { name: "Igbudu Monkey", id: "gh4" }
-    ]
+    this.state = {
+      monsters: [
+        { name: "Frankeistein", id: "234" },
+        { name: "Dracula", id: "23" },
+        { name: "Pompei", id: "er32" },
+        { name: "Chockee", id: "23ew" },
+        { name: "Zombie", id: "21wer" },
+        { name: "Igbudu Monkey", id: "gh4" }
+      ]
+    };
   }
-}
   // componentDidMount() {
-    // fetch("https://jsonplaceholder.typicode.com/users")
-    //   .then(response => response.json())
-    //   .then(formattedData => this.setState({ monsters: formattedData }));
+  // fetch("https://jsonplaceholder.typicode.com/users")
+  //   .then(response => response.json())
+  //   .then(formattedData => this.setState({ monsters: formattedData }));
   // }
 
   render() {
-    return this.state.monsters.map(monster => (
-      <CardList monsters = {this.state.monsters} >
-        <h3>
-          {monster.name}
-        </h3>
-      </CardList>
-    ));
+    return (
+      <div className="card-list">
+        {this.state.monsters.map(monster => (
+          <CardList monsters={this.state.monsters} key={monster.id}>
+            <h3>{monster.name}</h3>
+          </CardList>
+        ))}
+      </div>
+    );
   }
 }
 
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,5 +2,7 @@ import React from "react";
 import "./card-list.css";
 
 export const CardList = props => (
-  <div className="card-list"> {props.children} </div>
+    
+  <div> {props.children} </div> 
+  
 );
